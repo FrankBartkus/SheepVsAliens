09c004f67b45bcbdf6336cf12fc9bb10b94dfada
himynameisissy <67871852+himynameisissy@users.noreply.github.com>
2020-07-30 13:49:32 -0700

    Quitting the game
    Quits the game with ESC. Fairly self-explanatory, really.

---
+++ SheepVsAliens/Assets/Scripts/QuitWithEsc.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitWithEsc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //There's not anything in the start function to do. I mean, is the average player gonna throw a hissy fit that they can't exit the 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Now we can actually start quitting the game! We're gonna have it so you press ESC to quit.
+        //Do keep in mind that this code will NOT work without building the scene first.
+        //Pressing ESC while just running it will do nothing.
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        //That makes it so pressing ESC quits the application (aka the game).
+    }
+}
