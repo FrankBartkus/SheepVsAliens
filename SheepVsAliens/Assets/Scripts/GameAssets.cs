446ef623fbc20d1fc93df2a58e7460edcc4df8eb
Grape In Need Of Surgery <45608780+FrankBartkus@users.noreply.github.com>
2020-07-30 16:09:17 -0700

    push

--- SheepVsAliens/Assets/Scripts/GameAssets.cs
+++ SheepVsAliens/Assets/Scripts/GameAssets.cs
@@ -15,6 +15,8 @@ public class GameAssets : MonoBehaviour
 
     public Sprite rangeSprite;
     public Sprite checkSprite;
+    public Sprite alienHor;
+    public Sprite alienVert;
 
     public SoundAudioClip[] soundAudioClipArray;
 
