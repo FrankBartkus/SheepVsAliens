5f5fbf7dbf80efd676401d641dc07b904604f6a9
Grape In Need Of Surgery <45608780+FrankBartkus@users.noreply.github.com>
2020-07-30 14:49:11 -0700

    level

---
+++ SheepVsAliens/Assets/Scripts/NewLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLevel : MonoBehaviour
+{
+    public void newLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    public void newLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
